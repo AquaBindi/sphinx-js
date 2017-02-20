f3232a512be904548516e618517a0da057cb6ae7
Toshiharu Moriyama <aqua.bindi@gmail.com>
2017-02-20 19:04:12 +0900

    recurse into subdirectories when scanning for source files

--- sphinx_js/jsdoc.py
+++ sphinx_js/jsdoc.py
@@ -5,7 +5,7 @@ from subprocess import check_output
 def run_jsdoc(app):
     """Run JSDoc across a whole codebase, and squirrel away its results."""
     # JSDoc defaults to utf8-encoded output.
-    doclets = loads(check_output(['jsdoc', app.config.js_source_path, '-X']).decode('utf8'))
+    doclets = loads(check_output(['jsdoc', app.config.js_source_path, '-r', '-X']).decode('utf8'))
     app._sphinxjs_jsdoc_output = dict((d['longname'], d) for d in doclets
                                       if d.get('comment')
                                       and not d.get('undocumented'))
